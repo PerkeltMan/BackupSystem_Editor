a851d6de49ddca415d89917981e4f5098e00474e
Vít Müller <vit.mueller@seznam.cz>
2026-03-01 20:01:31 +0100

    finished textbox

--- Editor/Components/Non-WindowComponents/Textbox.cs
+++ Editor/Components/Non-WindowComponents/Textbox.cs
@@ -8,6 +8,7 @@ namespace Editor.Components.Components
     {
         private string label;
         private string value;
+        private Dictionary<ConsoleKey, Action> keys = new Dictionary<ConsoleKey, Action>();
 
         public event Action<string>? ValueChanged;
 
@@ -15,16 +16,42 @@ namespace Editor.Components.Components
         {
             this.label = label;
             this.value = value;
+
+            this.keys[ConsoleKey.Backspace] = this.Backspace;
+            this.keys[ConsoleKey.Enter] = this.Enter;
         }
 
         public void Draw()
         {
-            
+            Console.WriteLine(this.label);
+            Console.WriteLine("[ " + this.value + " ]");
         }
 
         public void HandleKey(ConsoleKeyInfo info)
         {
-            throw new NotImplementedException();
+            if (this.keys.ContainsKey(info.Key))
+            {
+                this.keys[info.Key]();
+            }
+            else if (!char.IsControl(info.KeyChar))
+            {
+                this.value += info.KeyChar;
+                this.ValueChanged?.Invoke(this.value);
+            }
+        }
+
+        private void Backspace()
+        {
+            if (this.value.Length > 0)
+            {
+                this.value = this.value.Substring(0, this.value.Length - 1);
+                this.ValueChanged?.Invoke(this.value);
+            }
+        }
+
+        private void Enter()
+        {
+            // Do nothing for now
         }
     }
 }
